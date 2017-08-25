d545b00ae5ee9595f522480ebf21797699466223
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-08-25 11:23:50 -0700

    Create a squad model to store the name and description of the team

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Squad } from '../squad.model';
 
 @Component({
   selector: 'app-teams',
@@ -6,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teams.component.css']
 })
 export class TeamsComponent implements OnInit {
+
+  squads: Squad[] = [
+    new Squad("Hacking Squad",
+       "It is a team with passionate hacker cum coders", 1),
+   new Squad("Bits Magnet",
+      "It is a team with passionate hacker cum coders", 2),
+  new Squad("Jammers",
+     "It is a team with passionate hacker cum coders", 3),
+
+
+
+
+
+
+
+
+
+
+
+  ];
 
   constructor() { }
 
